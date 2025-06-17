9d835f7fc46288b4cc1de57fad4b4e53886d0daf
arhangelskij <shuron@mail.com>
2025-06-17 17:50:37 +0300

    Сlarify bip39 usage and remove outdated comment

--- solana-tools-lite/src/crypto/bip39.rs
+++ solana-tools-lite/src/crypto/bip39.rs
@@ -1,13 +1,17 @@
 use hmac::Hmac;
 use pbkdf2::pbkdf2;
 use sha2::Sha512;
+use bip39::{Mnemonic, Language};
 
+/// Generate a random 12-word English BIP-39 mnemonic phrase.
 pub fn generate_mnemonic() -> String {
-    // TODO: tmp
-    "abandon abandon abandon abandon abandon abandon abandon abandon abandon abandon abandon about"
-        .into()
+    let mut rng = bip39::rand::thread_rng();
+    let mnemonic = Mnemonic::generate_in_with(&mut rng, Language::English, 12).unwrap();
+    
+    mnemonic.to_string()
 }
 
+/// Derive a 64-byte seed from a BIP-39 mnemonic and passphrase.
 pub fn derive_seed(mnemonic: &str, passphrase: &str) -> [u8; 64] {
     let salt = format!("mnemonic{}", passphrase);
     let mut out = [0u8; 64];
