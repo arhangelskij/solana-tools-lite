341c3679c47b08385aeba66a7da1ce9bc85402bc
arhangelskij <3616801+arhangelskij@users.noreply.github.com>
2025-08-11 15:38:36 +0300

    Update generation.rs

--- solana-tools-lite/src/flows/generation.rs
+++ solana-tools-lite/src/flows/generation.rs
@@ -1,3 +1,4 @@
+use std::path::Path;
 use crate::errors::ToolError;
 use crate::layers::io::write_output;
 use crate::models::results::GenResult;
@@ -14,37 +15,37 @@ pub fn execute(
     json: bool,
     show_secret: bool,
     out_path: Option<&str>,
+    force: bool
 ) -> Result<(), ToolError> {
-    save_to_file(result, out_path)?;
+    save_to_file(result, out_path, force)?;
     print_result(result, json, show_secret)?;
 
     Ok(())
 }
 
 fn print_result(result: &GenResult, json: bool, show_secret: bool) -> Result<(), ToolError> {
-    if !show_secret {
-        let public_gen_result = result.as_public();
-        if json {
-            // Pretty JSON only with public fields
-            pretty_print_json(&public_gen_result);
-        } else {
-            println!("{}", public_gen_result);
-        }
-    } else {
-        if json {
-            // Pretty JSON with secrets
-            pretty_print_json(&result);
-        } else {
-            println!("{}", result);
-        }
+    match (json, show_secret) {
+        // Pretty JSON with secrets
+        (true, true ) => pretty_print_json(result),
+        // With secrets
+        (false, true ) => println!("{}", result),
+        // Public
+        (true, false) => pretty_print_json(&result.as_public()),
+        (false, false) => println!("{}", result.as_public())
+        
     }
-
     Ok(())
 }
 
-fn save_to_file(result: &GenResult, out_path: Option<&str>) -> Result<(), ToolError> {
+fn save_to_file(result: &GenResult, out_path: Option<&str>, force: bool) -> Result<(), ToolError> {
     // Default file name when user didn't provide one
     let wallet_path = out_path.unwrap_or("wallet.json");
+
+    // If file exists and not forced, return an error
+    if Path::new(wallet_path).exists() && !force {
+        return Err(ToolError::FileExists(wallet_path.to_string()));
+    }
+
     // Always save full wallet to file
     write_output(Some(wallet_path), &result.to_full_json())?;
 
